5b25ec054ed917dab1f8590abd051a66864d5643
Marcelo Vogt <marcelinho.vogt@gmail.com>
2018-10-02 19:54:00 -0300

    Mudanças de cena

---
+++ Assets/MudarCena.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MudarCena : MonoBehaviour {
+
+    public void GoToWrite() {
+        SceneManager.LoadScene("Escrever");
+    }
+
+    public void GoToTranslate() {
+        SceneManager.LoadScene("Traduzir");
+    }
+}
